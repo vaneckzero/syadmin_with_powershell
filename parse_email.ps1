afa12c5ea9f90868f5fe538b211f2c3d9a9faa24
Nick Hall <nick@vaneckzero.com>
2021-10-18 13:45:57 -0500

    test

--- parse_email.ps1
+++ parse_email.ps1
@@ -9,4 +9,4 @@ write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Sender---
 Get-Content $email | Where-Object {$_ -like ‘*sender*’}
 
 #get iocs (urls, filenames, hashes, etc) and enrich with VT, etc
-#just read file once and then parse variable
+#just read file once and then parse variables
