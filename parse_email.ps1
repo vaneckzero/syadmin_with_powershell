cff9549783997ef7977528ac172e7472f892a4dc
Nick Hall <nick@vaneckzero.com>
2021-10-18 13:44:11 -0500

    created parse_email.ps1

---
+++ parse_email.ps1
@@ -0,0 +1,12 @@
+$email = $args[0]
+write-host -BackgroundColor green -foregroundcolor DarkBlue "----------To:----------"
+Get-Content $email | Where-Object {$_ -like ‘*To:*’}
+write-host -BackgroundColor green -foregroundcolor DarkBlue "----------From:----------"
+Get-Content $email | Where-Object {$_ -like ‘*From:*’}
+write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Received----------"
+Get-Content $email | Where-Object {$_ -like ‘Received:*’}
+write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Sender----------"
+Get-Content $email | Where-Object {$_ -like ‘*sender*’}
+
+#get iocs (urls, filenames, hashes, etc) and enrich with VT, etc
+#just read file once and then parse variable
