bebddc3f10fd27449ee1ad62a34ea8855de1a5a4
Nick Hall <nick@vaneckzero.com>
2021-10-18 16:55:17 -0500

    reading file once, formatting output

--- parse_email.ps1
+++ parse_email.ps1
@@ -1,14 +1,24 @@
-$email = $args[0]
+$email = get-content $args[0]
+
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------To:----------"
-Get-Content $email | Where-Object {$_ -like ‘To:*’}
+    $email | Where-Object {$_ -like ‘To:*’}
+    write-host "`n"
+
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------From:----------"
-Get-Content $email | Where-Object {$_ -like ‘From:*’}
+    $email | Where-Object {$_ -like ‘From:*’}
+    write-host "`n"
+
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Received----------"
-Get-Content $email | Where-Object {$_ -like ‘Received:*’}
+    $email | Where-Object {$_ -like ‘Received:*’}
+    write-host "`n"
+
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Sender----------"
-Get-Content $email | Where-Object {$_ -like ‘*sender*’}
+    $email | Where-Object {$_ -like ‘*sender*’}
+    write-host "`n"
+
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Message Info----------"
-Get-Content $email | select-string ‘message_boundary’ -Context 0,4
+    $email | select-string ‘message_boundary’ -Context 0,4
+    write-host "`n"
 
 # should read file once and then parse variables
 # get iocs (urls, filenames, hashes, etc) and enrich with VT, etc https://developers.virustotal.com/reference#files-scan
