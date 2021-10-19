35baf18b30bc9e17a6dac5872b827dc1fbcb21d9
Nick Hall <nick@vaneckzero.com>
2021-10-19 12:01:00 -0500

    changed static ip in ping_forever to target variable

--- ping_forever.ps1
+++ ping_forever.ps1
@@ -2,6 +2,6 @@ $target = $args[0]
  while (1 -eq 1){
         get-date >> ping_$target.txt
         write-host "`n"
-        ping 192.168.0.1 >> ping_$target.txt
+        ping $target >> ping_$target.txt
         write-host "`n"
     }
