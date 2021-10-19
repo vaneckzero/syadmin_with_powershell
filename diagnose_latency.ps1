6af1969cd7cf27f6b742ab7bc54f3e594c3980fd
Nick Hall <nick@vaneckzero.com>
2021-10-19 10:40:16 -0500

    added user input to diagnose_latency

--- diagnose_latency.ps1
+++ diagnose_latency.ps1
@@ -1,8 +1,8 @@
-$target = "8.8.8.8"
+$target = $args[0]
 while($true)
 {
      $i++
-     write-host "starting loop $i"
+     write-host -BackgroundColor red -foregroundcolor white "tracing target: $target ----- starting loop $i"
      echo "---------------------Loop:$i---------------------------" >> trace_$target.txt
      get-date >> trace_$target.txt
      tracert $target >> trace_$target.txt
