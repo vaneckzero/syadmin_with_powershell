3d187abbe8ec1ef7454cff7941118946c50a382d
Nick Hall <nick@vaneckzero.com>
2021-10-27 16:37:08 -0500

    usage info added to diagnose_latency

--- diagnose_latency.ps1
+++ diagnose_latency.ps1
@@ -1,10 +1,15 @@
 $target = $args[0]
-while($true)
-{
+if (!$target){
+     write-host "Usage: diagnose_latency.ps1 targetip"
+     write-host "Will run continuous timestamped traceroutes to targetip and output trace_targetip.txt in same directory"
+ }
+ else {
+while($true){
      $i++
      write-host -BackgroundColor red -foregroundcolor white "tracing target: $target ----- starting loop $i"
      echo "---------------------Loop:$i---------------------------" >> trace_$target.txt
      get-date >> trace_$target.txt
      tracert $target >> trace_$target.txt
      sleep 5
-}
+     }
+ }
