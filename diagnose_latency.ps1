7c36ccab34fbb4773dd180119594ff8828736663
Nick Hall <nick@vaneckzero.com>
2021-10-27 16:38:47 -0500

    removing deprecated command aliases

--- diagnose_latency.ps1
+++ diagnose_latency.ps1
@@ -7,9 +7,9 @@ if (!$target){
 while($true){
      $i++
      write-host -BackgroundColor red -foregroundcolor white "tracing target: $target ----- starting loop $i"
-     echo "---------------------Loop:$i---------------------------" >> trace_$target.txt
+     write-output "---------------------Loop:$i---------------------------" >> trace_$target.txt
      get-date >> trace_$target.txt
      tracert $target >> trace_$target.txt
-     sleep 5
+     start-sleep 5
      }
  }
