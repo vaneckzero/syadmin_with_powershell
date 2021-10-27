a829dd341994938d0cdac6659394e4a062acd1db
Nick Hall <nick@vaneckzero.com>
2021-10-27 10:44:47 -0500

    formatting

--- parse_email.ps1
+++ parse_email.ps1
@@ -5,29 +5,23 @@ $email = get-content $args[0]
 
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------To:----------"
     $email | Where-Object {$_ -like ‘To:*’}
-    write-host "`n"
 
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------From:----------"
     $email | Where-Object {$_ -like ‘From:*’ -or $_ -like 'Return-Path:*'}
-    write-host "`n"
 
     write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Subject:----------"
     $email | Where-Object {$_ -like ‘Subject:*’}
-    write-host "`n"
 
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Received----------"
     $email | Where-Object {$_ -like ‘Received:*’}
-    write-host "`n"
 
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Sender----------"
     $email | Where-Object {$_ -like ‘*sender*’}
     $senders = $email | Where-Object {$_ -like ‘*sender*’}
     $sender_ioc = $senders[0].Split(' ')[1]
-    write-host "`n"
 
 write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Message Content----------"
     $email | select-string ‘Content-type: ’ -Context 0,3
-    write-host "`n"
 
 # get iocs (urls, filenames, hashes, etc) and enrich with VT, etc https://developers.virustotal.com/reference#files-scan
 # help comment block with parameters, etc
