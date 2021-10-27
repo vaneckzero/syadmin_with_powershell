56e29d0d6a740e6333a49f15cec5358bf844e6e3
Nick Hall <nick@vaneckzero.com>
2021-10-27 10:51:58 -0500

    formatting and usage for ping_forever

--- ping_forever.ps1
+++ ping_forever.ps1
@@ -1,7 +1,14 @@
 $target = $args[0]
- while (1 -eq 1){
+if (!$target){
+    write-host "Usage: ping_forever.ps1 targetip"
+    write-host "Will output ping_targetip.txt in same folder and continuously timestamp and ping 4 times"
+}
+else {
+    while (1 -eq 1){
         get-date >> ping_$target.txt
         write-host "`n"
         ping $target >> ping_$target.txt
         write-host "`n"
-    }
+    }   
+}
+ 
