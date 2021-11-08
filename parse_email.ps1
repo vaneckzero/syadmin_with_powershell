229578468f0f8e15a79c9386c9441218d92dd612
Nick Hall <nick@vaneckzero.com>
2021-11-08 15:45:47 -0600

    adding Censys API sender search to parse_email

--- parse_email.ps1
+++ parse_email.ps1
@@ -1,3 +1,6 @@
+$censys_id = ""
+$censys_key = ""
+
 write-host -BackgroundColor White -ForegroundColor DarkGreen "parse_email.ps1"
 write-host -BackgroundColor White -ForegroundColor DarkGreen "Nick Hall @vaneckzero (2021)`n"
 
@@ -33,6 +36,18 @@ write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Links----
     $email | select-string ‘href’ -Context 0,3
     #$email | Where-Object {$_ -like ‘*img src*’}
     #$email | Where-Object {$_ -like ‘*href*’}
+
+write-host -BackgroundColor green -foregroundcolor DarkBlue "----------Censys Sender Details----------"
+    if ($censys_id -eq ""){
+        write-host "Censys API ID and Key not found. See https://censys.io/api for more info"
+    }
+    else {
+        $ipintel = curl --silent https://censys.io/api/v1/view/ipv4/$sender_ioc -u ${censys_id}:${censys_key} | ConvertFrom-Json
+        #write-host "https://censys.io/api/v1/view/ipv4/$sender_ioc -u ${censys_id}:${censys_key}"
+            write-host "Sender Location: " $ipintel.location.country $ipintel.location.province 
+            write-host "AS Info: " $ipintel.autonomous_system.country_code $ipintel.autonomous_system.routed_prefix
+    }        
 }
+
 # get iocs (urls, filenames, hashes, etc) and enrich with VT, etc https://developers.virustotal.com/reference#files-scan
 # help comment block with parameters, etc
