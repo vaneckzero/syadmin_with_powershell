45eb03748e071249c89595005910d5b88a039a88
Nick Hall <nick@vaneckzero.com>
2022-01-26 14:34:38 -0600

    creating shodan_search.ps1

---
+++ shodan_search.ps1
@@ -0,0 +1,6 @@
+######################################################
+# - connect to netgate firewalls and retrieve WAN IP #
+# - seach WAN IP via Shodan API, return JSON         #
+# - parse and store results, send notification       #
+# - schedule                                         #
+######################################################
