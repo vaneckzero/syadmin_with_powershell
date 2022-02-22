586bc32d3406a3dedd9b4f85a04d2345d3cdd024
Nick Hall <nhall@westan.local>
2022-02-22 16:17:20 -0600

    worrking on msi removal and install automation

---
+++ msi_remove_screenconnect.ps1
@@ -0,0 +1,14 @@
+##############################################
+#  remove old screenconnect and install new  #
+##############################################
+
+#find msi guids
+$sc19guid = (get-wmiobject Win32_Product | where Name -like "*screenconnect*" | where LocalPackage -notlike "*c:\windows*").IdentifyingNumber
+$sc21guid = (get-wmiobject Win32_Product | where Name -like "*screenconnect*" | where LocalPackage -like "*c:\windows*").IdentifyingNumber
+
+#uninstall both 
+msiexec.exe /x "$sc19guid" /QN /L*V "C:\sc_uninstall.log" REBOOT=R
+msiexec.exe /x "$sc21guid" /QN /L*V "C:\sc_uninstall.log" REBOOT=R
+
+#install 22.1
+msiexec /i "\\ucnas\netshare\ConnectWise Installers\westan_connectwise_02220222.msi" /quiet
