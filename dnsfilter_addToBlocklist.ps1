ad0c24dcc895ad4450b35cb0acae818dcc2c58b5
Nick Hall <nick@vaneckzero.com>
2024-01-08 12:08:45 -0600

    interpolating includes correctly

--- dnsfilter_addToBlocklist.ps1
+++ dnsfilter_addToBlocklist.ps1
@@ -6,7 +6,7 @@
 #write-output "Pass: $dnsfilter_pass"
 
 #authentication and authorization w/ DNSFilter API
-$auth_postParams = @{grant_type='http://auth0.com/oauth/grant-type/password-realm';client_id='zJ1WJHavuUFx89cConwlipxoOc2J3TVQ';realm='Username-Password-Authentication';audience='https://dnsfilter.auth0.com/mfa/';scope='enroll read:authenticators remove:authenticators offline_access openid picture name email';username='$dnsfilter_user';password='$dnsfilter_pass'}
+$auth_postParams = @{grant_type='http://auth0.com/oauth/grant-type/password-realm';client_id='zJ1WJHavuUFx89cConwlipxoOc2J3TVQ';realm='Username-Password-Authentication';audience='https://dnsfilter.auth0.com/mfa/';scope='enroll read:authenticators remove:authenticators offline_access openid picture name email';username="$dnsfilter_user";password="$dnsfilter_pass"}
 Write-Output $auth_postParams
 
 #$dnsfilter_authRequest = Invoke-WebRequest -Uri https://dnsfilter.auth0.com/oauth/token -Method POST -Body $auth_postParams
