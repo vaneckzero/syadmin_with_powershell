233740a46da75af8540cf4ff6d4c9a80e1f6623e
Nick Hall <nick@vaneckzero.com>
2024-01-08 12:07:07 -0600

    created dnsfilter_addtoblocklist.ps, beginning work on auth

---
+++ dnsfilter_addToBlocklist.ps1
@@ -0,0 +1,13 @@
+#include dnsfilter_includes.ps1, need to set dnsfilter_user and dnsfilter_pass
+. .\dnsfilter_includes.ps1
+
+#testing credential inclusions
+#write-output "User: $dnsfilter_user"
+#write-output "Pass: $dnsfilter_pass"
+
+#authentication and authorization w/ DNSFilter API
+$auth_postParams = @{grant_type='http://auth0.com/oauth/grant-type/password-realm';client_id='zJ1WJHavuUFx89cConwlipxoOc2J3TVQ';realm='Username-Password-Authentication';audience='https://dnsfilter.auth0.com/mfa/';scope='enroll read:authenticators remove:authenticators offline_access openid picture name email';username='$dnsfilter_user';password='$dnsfilter_pass'}
+Write-Output $auth_postParams
+
+#$dnsfilter_authRequest = Invoke-WebRequest -Uri https://dnsfilter.auth0.com/oauth/token -Method POST -Body $auth_postParams
+#write-output $dnsfilter_authRequest
