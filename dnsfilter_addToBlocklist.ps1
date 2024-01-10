ab5ab154800d40ccde363630721c73eb204e0eb4
Nick Hall <nick@vaneckzero.com>
2024-01-10 11:20:24 -0600

    adding url parameter parsing and logic to search DNSFilter blocklist for url

--- dnsfilter_addToBlocklist.ps1
+++ dnsfilter_addToBlocklist.ps1
@@ -1,3 +1,6 @@
+#retrieve url paramenter from command
+param($url)
+
 #include dnsfilter_includes.ps1, need to set dnsfilter_user and dnsfilter_pass
 . .\dnsfilter_includes.ps1
 
@@ -10,6 +13,8 @@ $auth_postParams = @{grant_type='http://auth0.com/oauth/grant-type/password-real
 #Write-Output $auth_postParams
 $dnsfilter_authRequest = Invoke-WebRequest -Uri https://dnsfilter.auth0.com/oauth/token -Method POST -Body $auth_postParams | ConvertFrom-Json
 $dnsfilter_accessToken = $dnsfilter_authRequest.access_token
+Write-Host "DNSFilter Auth Token:" -ForegroundColor Green
+Write-Host "$dnsfilter_accessToken `n"
 
 #query DNSFilter blockslists for domain, 181610 is the hardcode policy ID we're searching for this org
 $search_headers = @{
@@ -18,7 +23,9 @@ $search_headers = @{
 }
 $dnsfilter_domainQuery = Invoke-WebRequest -Uri https://api.dnsfilter.com/v1/policies?include_global_policies=false -Method GET -Headers $search_headers | ConvertFrom-Json
 #$dnsfilter_domainQuery.data | Where-Object { $_.id -eq 181610 -and $_.attributes.blacklist_domains -like "fixcde.com"}
-if ($dnsfilter_domainQuery.data.attributes[1].blacklist_domains -like "fixsdfsdfcde.com") #attributes[1] is the second policy in our tenant which is the one I want to search
-    {write-output "domain found"}
+if ($dnsfilter_domainQuery.data.attributes[1].blacklist_domains -like "$url") #attributes[1] is the second policy in our tenant which is the one I want to search
+    {write-host "domain $url found in DNSFilter blocklist" -ForegroundColor Green}
 else 
-    {write-output "domain not found" }
+    {write-host "domain $url not found in DNSFilter blocklist - adding..." -ForegroundColor Red
+    
+ }
